8c09f3c4f54e31dce289841222ebcb529cd2e636
Joe <joe@jjssoftware.co.uk>
2021-05-10 23:30:42 +0100

    reworked gulpfile for gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp        = require('gulp'),
+const { src, dest, watch, series } = require('gulp'),
 	plumber     = require('gulp-plumber'),
 	browserSync = require('browser-sync'),
 	stylus      = require('gulp-stylus'),
@@ -18,79 +18,91 @@ var messages = {
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
+function doJekyllBuild(done) {
 	browserSync.notify(messages.jekyllBuild);
-	return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+	cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
 		.on('close', done);
-});
+	done();
+}
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+function doJjekyllRebuild(done) {
+	jekyllBuild(done);
 	browserSync.reload();
-});
+	done();
+}
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+function doBrowserSync(done) {
+	doJekyllBuild(done);
 	browserSync({
 		server: {
-			baseDir: '_site'
+			baseDir: '../bebo-dot-dev.github.io'
 		}
 	});
-});
+	done();
+}
 
 /**
  * Stylus task
  */
-gulp.task('stylus', function(){
-		gulp.src('src/styl/main.styl')
+function doStylus(done) {
+	src('src/styl/main.styl')
 		.pipe(plumber())
 		.pipe(stylus({
 			use:[koutoSwiss(), prefixer(), jeet(),rupture()],
 			compress: true
 		}))
-		.pipe(gulp.dest('_site/assets/css/'))
-		.pipe(browserSync.reload({stream:true}))
-		.pipe(gulp.dest('assets/css'))
-});
+		.pipe(dest('../bebo-dot-dev.github.io/assets/css/'))
+		.pipe(browserSync.reload({stream:true}));
+	done();	
+}
 
 /**
  * Javascript Task
  */
-gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
+function doJs(done) {
+	src('src/js/**/*.js')
 		.pipe(plumber())
 		.pipe(concat('main.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
-});
+		.pipe(dest('../bebo-dot-dev.github.io/assets/js/'));
+	done();
+}
 
 /**
  * Imagemin Task
  */
-gulp.task('imagemin', function() {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
+function doImageMin(done) {
+	src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
-});
+		.pipe(dest('../bebo-dot-dev.github.io/assets/img/'));
+	done();
+}
 
 /**
  * Watch stylus files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-	gulp.watch('src/styl/**/*.styl', ['stylus']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
+function doWatch(done) {
+	watch('src/styl/**/*.styl', doStylus);
+	watch('src/js/**/*.js', doJs);
+	watch('src/img/**/*.{jpg,png,gif}', doImageMin);
+	watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], doJjekyllRebuild);
+}
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+ exports.default = function(done) {
+	series(doStylus, doJs, doImageMin, doBrowserSync, doWatch);
+	done();
+ } 
+ exports.doStylus = doStylus;
+ exports.build = series(doStylus, doJs, doImageMin, doBrowserSync, doWatch);
