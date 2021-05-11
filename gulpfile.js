28f2fbc27ac6bab264030d3382bb06e2ec18b1de
Joe <joe@jjssoftware.co.uk>
2021-05-11 08:07:24 +0100

    gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -28,8 +28,8 @@ function doJekyllBuild(done) {
 /**
  * Rebuild Jekyll & do page reload
  */
-function doJjekyllRebuild(done) {
-	jekyllBuild(done);
+function doJekyllRebuild(done) {
+	doJekyllBuild(done);
 	browserSync.reload();
 	done();
 }
@@ -78,7 +78,7 @@ function doJs(done) {
  * Imagemin Task
  */
 function doImageMin(done) {
-	src('src/img/**/*.{jpg,png,gif}')
+	src('assets/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 		.pipe(dest('../bebo-dot-dev.github.io/assets/img/'));
@@ -93,16 +93,12 @@ function doWatch(done) {
 	watch('src/styl/**/*.styl', doStylus);
 	watch('src/js/**/*.js', doJs);
 	watch('src/img/**/*.{jpg,png,gif}', doImageMin);
-	watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], doJjekyllRebuild);
+	watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], doJekyllRebuild);
 }
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
- exports.default = function(done) {
-	series(doStylus, doJs, doImageMin, doBrowserSync, doWatch);
-	done();
- } 
- exports.doStylus = doStylus;
- exports.build = series(doStylus, doJs, doImageMin, doBrowserSync, doWatch);
+ exports.default = series(doStylus, doJs, doImageMin, doBrowserSync, doWatch);
+ exports.build = series(doStylus, doJs, doImageMin, doJekyllBuild);
